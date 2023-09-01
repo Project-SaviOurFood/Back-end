6c1081618566bebddfa8d23f57cad83255b946e5
Brayan Santos <brayan.santosrosa@gmail.com>
2023-09-01 10:54:37 -0300

    Projeto Integrador Sav<i>OurFood creado.

---
+++ src/main/java/com/generation/SaviOurFood/SaviOurFoodApplication.java
@@ -0,0 +1,13 @@
+package com.generation.SaviOurFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaviOurFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaviOurFoodApplication.class, args);
+	}
+
+}
