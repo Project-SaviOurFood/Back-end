23d831f24f786d6e569e0a7f5a1468273617f7ed
Brayan Santos <brayan.santosrosa@gmail.com>
2023-10-18 12:28:10 -0300

    Date for string attribute expirationDate

--- src/main/java/com/generation/SaviOurFood/model/Product.java
+++ src/main/java/com/generation/SaviOurFood/model/Product.java
@@ -21,7 +21,7 @@ public class Product {
   private Float value;
 
   @NotNull(message = "The attribute date is mandatory")
-  private Date expirationDate;
+  private String expirationDate;
 
   private String picture;
 
@@ -56,11 +56,11 @@ public class Product {
     this.value = value;
   }
 
-  public Date getExpirationDate() {
+  public String getExpirationDate() {
     return expirationDate;
   }
 
-  public void setExpirationDate(Date expirationDate) {
+  public void setExpirationDate(String expirationDate) {
     this.expirationDate = expirationDate;
   }
 
